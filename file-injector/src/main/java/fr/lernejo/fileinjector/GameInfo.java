0cd9afbac4ca001094a0d4d5078fa9e4d49b8cfc
Myriam Ward <myriam.ward@epita.fr>
2023-11-30 10:34:34 +0100

    part 3: fichier and AMQP

--- file-injector/src/main/java/fr/lernejo/fileinjector/GameInfo.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/GameInfo.java
@@ -1,2 +1,13 @@
-package fr.lernejo.fileinjector;public record GameInfo() {
+package fr.lernejo.fileinjector;
+public record GameInfo(
+    int id,
+    String title,
+    String thumbnail,
+    String short_description,
+    String genre,
+    String platform,
+    String publisher,
+    String developer,
+    String release_date
+) {
 }
--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,47 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.rabbitmq.client.MessageProperties;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
 
-            System.out.println("Hello after starting Spring");
+            if (args.length > 0) {
+
+                File inputFile = Paths.get(args[0]).toFile();
+                ObjectMapper objectMapper = new ObjectMapper();
+                List<GameInfo> Gameinfo = Arrays.asList(objectMapper.readValue(inputFile, GameInfo.class));
+
+
+                RabbitTemplate rabbitTemplate = new RabbitTemplate();
+                rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+
+                for (GameInfo gameInfo : Gameinfo) {
+                    MessagePostProcessor messagePostProcessor = message -> {
+                        message.getMessageProperties().getHeaders().put("game_id", gameInfo.id());
+                        return message;
+                    };
+                    rabbitTemplate.convertAndSend("game_info", "", gameInfo, messagePostProcessor);
+                }
+            }
+        } catch (IOException err) {
+            throw new IOException(err);
         }
     }
 }
